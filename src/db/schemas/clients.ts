65f4c84f73d7fb93d17b9e14181383c4a4fb4731
Luís Gabriel Marchió Batista <63173708+LuisMarchio03@users.noreply.github.com>
2023-12-29 17:45:03 -0300

    Add clients table schema

---
+++ src/db/schemas/clients.ts
@@ -0,0 +1,13 @@
+import { createId } from '@paralleldrive/cuid2'
+import { pgTable, integer, varchar, text, date } from 'drizzle-orm/pg-core'
+
+export const clients = pgTable('clients', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  name: varchar('name').notNull(),
+  cnpj: varchar('cnpj').notNull(), // Login com cnpj
+  opening_date: date('opening_date').notNull(),
+  legal_entity_name: varchar('legal_entity_name').notNull(),
+  password: varchar('password').notNull(), // e Password
+})
