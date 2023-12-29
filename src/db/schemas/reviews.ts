ada0718d2b921d45424b2b21ee3ffcb9153689d5
Luís Gabriel Marchió Batista <63173708+LuisMarchio03@users.noreply.github.com>
2023-12-29 16:31:32 -0300

    Add reviews schema and relations

---
+++ src/db/schemas/reviews.ts
@@ -0,0 +1,32 @@
+import { createId } from '@paralleldrive/cuid2'
+import { relations } from 'drizzle-orm'
+import { pgTable, integer, text } from 'drizzle-orm/pg-core'
+import { restaurants } from './restaurants'
+import { users } from './users'
+
+export const reviews = pgTable('reviews', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  restaurantId: integer('restaurant_id').references(() => restaurants.id, {
+    onDelete: 'set null',
+  }),
+  userId: integer('user_id').references(() => users.id, {
+    onDelete: 'set null',
+  }),
+  rating: integer('rating').notNull(),
+  comment: text('comment'),
+})
+
+export const reviewsRelations = relations(reviews, ({ one, many }) => ({
+  restaurant: one(restaurants, {
+    fields: [reviews.restaurantId],
+    references: [restaurants.id],
+    relationName: 'restaurant',
+  }),
+  user: one(users, {
+    fields: [reviews.userId],
+    references: [users.id],
+    relationName: 'user',
+  }),
+}))
