c92287f8706658c3dfca6d1315dce1891a828fc9
Luís Gabriel Marchió Batista <63173708+LuisMarchio03@users.noreply.github.com>
2023-12-29 16:21:11 -0300

    Add menus schema and relations

---
+++ src/db/schemas/menus.ts
@@ -0,0 +1,24 @@
+import { createId } from '@paralleldrive/cuid2'
+import { relations } from 'drizzle-orm'
+import { pgTable, integer, text, varchar, decimal } from 'drizzle-orm/pg-core'
+import { restaurants } from './restaurants'
+
+export const menus = pgTable('menus', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  restaurantId: integer('restaurant_id').references(() => restaurants.id, {
+    onDelete: 'set null',
+  }),
+  dish: varchar('dish').notNull(),
+  description: text('description'),
+  price: decimal('price').notNull(),
+})
+
+export const menusRelations = relations(menus, ({ one, many }) => ({
+  restaurant: one(restaurants, {
+    fields: [menus.restaurantId],
+    references: [restaurants.id],
+    relationName: 'restaurant',
+  }),
+}))
