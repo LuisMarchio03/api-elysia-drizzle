5efaa89f840246b55c68eb759030a89ccf6129f0
Luís Gabriel Marchió Batista <63173708+LuisMarchio03@users.noreply.github.com>
2023-12-29 16:31:28 -0300

    Add orders schema and relations

---
+++ src/db/schemas/orders.ts
@@ -0,0 +1,33 @@
+import { createId } from '@paralleldrive/cuid2'
+import { relations } from 'drizzle-orm'
+import { pgTable, integer, varchar, numeric, text, jsonb } from 'drizzle-orm/pg-core'
+import { restaurants } from './restaurants'
+import { users } from './users'
+
+export const orders = pgTable('orders', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  userId: integer('user_id').references(() => users.id, {
+    onDelete: 'set null',
+  }),
+  restaurantId: integer('restaurant_id').references(() => restaurants.id, {
+    onDelete: 'set null',
+  }),
+  status: varchar('status').notNull(),
+  total: numeric('total', { precision: 5, scale: 2 }).notNull(),
+  details: jsonb('details'),
+})
+
+export const ordersRelations = relations(orders, ({ one, many }) => ({
+  user: one(users, {
+    fields: [orders.userId],
+    references: [users.id],
+    relationName: 'user',
+  }),
+  restaurant: one(restaurants, {
+    fields: [orders.restaurantId],
+    references: [restaurants.id],
+    relationName: 'restaurant',
+  }),
+}))
