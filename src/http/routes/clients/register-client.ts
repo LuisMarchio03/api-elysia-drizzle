54dd4b55fd9e4cb0cb1f7e14d552dbe77bd25595
Luís Gabriel Marchió Batista <63173708+LuisMarchio03@users.noreply.github.com>
2024-01-03 17:56:26 -0300

    Add client registration endpoint and fetch data from external API

--- src/http/routes/clients/register-client.ts
+++ src/http/routes/clients/register-client.ts
@@ -1,10 +1,18 @@
 import { clients } from '@/db/schemas'
+import { configs } from '@/db/schemas/config'
 
 import { db } from '@/db/connection'
 import { z } from 'zod'
 
 import Elysia from 'elysia'
-import { configs } from '@/db/schemas/config'
+
+
+interface IDataCnpj {
+  razao_social: string,
+  simples: {
+    data_opcao_mei: string
+  }
+}
 
 const registerClientBodySchema = z.object({
   name: z.string(),
@@ -15,23 +23,37 @@ const registerClientBodySchema = z.object({
 export const registerClient = new Elysia().post(
   '/clients',
   async ({ body, set }) => {
-    const { cnpj, name, password } = registerClientBodySchema.parse(body)
+    try {
+      const { cnpj, name, password } = registerClientBodySchema.parse(body)
 
-    //TODO: Buscar legal_entity_name (Razão social) e opening_date (Data de abertura) -> Via api da receita federal com base no CNPJ
-    //TODO: Junto com a criação de um novo cliente, eu devo criar um table no banco de dados para armazenar as configurações desse cliente (WHITE-LABEL) 
+      const alreadyExists = await db.query.clients.findFirst({
+        where: {
+          cnpj: cnpj,
+        },
+      });
+      
+      if (alreadyExists) {
+        throw new Error("Client already exists")
+      }
 
+      const res = await fetch(`https://publica.cnpj.ws/cnpj/${cnpj}`)
+      const data: IDataCnpj = await res.json() as IDataCnpj
+      const { razao_social: legal_entity_name, simples: { data_opcao_mei: opening_date } } = data
+          
+      await db.insert(clients).values({
+        cnpj, legal_entity_name, name, opening_date, password
+      })
+    
+      //TODO: Junto com a criação de um novo cliente, eu devo criar um table no banco de dados para armazenar as configurações desse cliente (WHITE-LABEL) 
+      // console.log("created", created)
+      // await db.insert(configs).values({
+      //   company_name,
+      //   // clientId
+      // })
 
-    const created = await db.insert(clients).values({
-      cnpj, legal_entity_name, name, opening_date, password
-    })
-
-    console.log("created", created)
-
-    // await db.insert(configs).values({
-    //   company_name,
-    //   // clientId
-    // })
-
-    set.status = 401
+      set.status = 201
+    } catch {
+      set.status = 400
+    }
   },
 )
